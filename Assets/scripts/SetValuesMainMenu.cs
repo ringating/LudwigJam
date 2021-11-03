79f647f2acf3dba00a6628f0c1d54f8ecf44843f
Chris <34548930+ringating@users.noreply.github.com>
2021-11-03 03:44:32 -0700

    made planes not get stuck on plummeter
    also added a presumptuous native-res-selecting script

--- Assets/scripts/SetValuesMainMenu.cs
+++ Assets/scripts/SetValuesMainMenu.cs
@@ -5,5 +5,10 @@ public class SetValuesMainMenu : MonoBehaviour
     void Start()
     {
         StaticValues.hardMode = false;
+
+        // force what i assume is native res
+        Resolution[] resolutions =  Screen.resolutions;
+        Resolution theOne = resolutions[resolutions.Length - 1];
+        Screen.SetResolution(theOne.width, theOne.height, true);
     }
 }
